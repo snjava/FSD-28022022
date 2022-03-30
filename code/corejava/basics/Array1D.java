edc8692bbcfaae73aff455e6c785d9fba0b9beb2
scn-tech <scnaikwadi2@gmail.com>
2022-03-30 09:36:38 +0530

    Array 1-D
    Array 1-D

---
+++ code/corejava/basics/Array1D.java
@@ -0,0 +1,19 @@
+public class Array1D
+{
+	public static void main(String[] args)
+	{
+		int marks[]; // Declaration of Array
+		marks = new int[5]; // instance creation of Array
+
+		marks[1] = 60;
+		marks[2] = 70;
+		marks[4] = 80;
+
+		System.out.println(marks[0]); // 0
+		System.out.println(marks[1]); // 60
+		System.out.println(marks[2]); // 70
+		System.out.println(marks[3]); // 0
+		System.out.println(marks[4]); // 80
+		
+	}
+}
