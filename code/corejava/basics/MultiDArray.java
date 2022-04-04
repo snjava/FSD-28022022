e95f6d8387dab6ed6d152353e25688a491f89754
scn-tech <scnaikwadi2@gmail.com>
2022-04-04 11:06:16 +0530

    Multi-D Array
    Multi-D Array

---
+++ code/corejava/basics/MultiDArray.java
@@ -0,0 +1,21 @@
+public class MultiDArray
+{
+	public static void main(String args[])
+	{
+		int arr[][] = {{23,45,32,4},{324,45,6},{23,6},{56,87,56,78,98,45}};
+
+		System.out.println(arr.length); // number of Rows
+		System.out.println(arr[0].length); // number of column from 2nd row
+
+
+		for(int r = 0; r < arr.length ; r++)
+		{
+			for(int c = 0 ; c<arr[r].length ; c++)
+			{
+				System.out.print(arr[r][c] + "\t");
+			}
+			System.out.println();
+		}
+
+	}	
+}
