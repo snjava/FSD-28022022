f0cbe389a8f6046f44cb663ad753eee606dd9c32
scn-tech <scnaikwadi2@gmail.com>
2022-04-05 11:03:45 +0530

    Java Method Intro
    Java Method Intro

--- code/corejava/basics/MultiDArray.java
+++ code/corejava/basics/MultiDArray.java
@@ -5,7 +5,7 @@ public class MultiDArray
 		int arr[][] = {{23,45,32,4},{324,45,6},{23,6},{56,87,56,78,98,45}};
 
 		System.out.println(arr.length); // number of Rows
-		System.out.println(arr[0].length); // number of column from 2nd row
+		System.out.println(arr[1].length); // number of column from 2nd row
 
 
 		for(int r = 0; r < arr.length ; r++)
