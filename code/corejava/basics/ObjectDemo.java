e4458e257548564ed577b3f2c85ae5dcf5237fd0
scn-tech <scnaikwadi2@gmail.com>
2022-04-07 09:46:01 +0530

    Class Object, Memory Location
    Class Object, Memory Location

---
+++ code/corejava/basics/ObjectDemo.java
@@ -0,0 +1,26 @@
+public class ObjectDemo
+{
+	public static void main(String args[])
+	{
+		Employee emp; // reference variable
+		emp = new Employee(); // Object of Employee Class
+
+		emp.id = 121;   // call/access the variables using Object
+		emp.salary = 34534.43;
+
+		emp.displayEmployee(); // call/access the method using Object
+	}
+}
+
+
+class Employee
+{
+	int id;
+	double salary;
+
+	public void displayEmployee()
+	{
+		System.out.println("Id : " + id);
+		System.out.println("Salary : " + salary);
+	}
+}
