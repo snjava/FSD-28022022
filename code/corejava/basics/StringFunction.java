99b9a5d2dcb8b8b0e1168cc8abc793fa40e5e440
scn-tech <scnaikwadi2@gmail.com>
2022-04-13 11:10:26 +0530

    String Function Intro
    String Function Intro

---
+++ code/corejava/basics/StringFunction.java
@@ -0,0 +1,18 @@
+public class StringFunction
+{
+	public static void main(String args[])
+	{
+		String msg = "       Hello     Java   ";
+		System.out.println(msg); 
+		System.out.println(msg.toUpperCase()); 
+		System.out.println(msg.toLowerCase());
+		System.out.println(msg.trim()); 
+		System.out.println(msg);
+		System.out.println("==================");
+		String msg2 = "Welcome";
+		System.out.println(msg2);
+		System.out.println(msg2.length());
+		System.out.println(msg2.isEmpty());
+		
+	}
+}
