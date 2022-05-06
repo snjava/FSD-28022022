242fc211e40077141380a529d3835e3fe1fbfaf5
scn-tech <scnaikwadi2@gmail.com>
2022-05-06 09:49:22 +0530

    Access Modifier
    Access Modifier

--- code/corejava/workspace/package-acess-modifier/src/admin/AdminReport.java
+++ code/corejava/workspace/package-acess-modifier/src/admin/AdminReport.java
@@ -1,12 +1,12 @@
-package student;
+package admin;
 
-public class StudentAttendance extends StudentInfo {
+import student.StudentInfo;
 
-	
+public class AdminReport extends StudentInfo {
 	public void print() {
 		System.out.println(id);
 		System.out.println(name);
-		System.out.println(contact);
+		//System.out.println(contact);
 		//System.out.println(age);
 	}
 	
@@ -14,8 +14,8 @@ public class StudentAttendance extends StudentInfo {
 	public void printByObject() {
 		StudentInfo st = new StudentInfo();
 		System.out.println(st.id);
-		System.out.println(st.name);
-		System.out.println(st.contact);
+		//System.out.println(st.name);
+		//System.out.println(st.contact);
 		//System.out.println(st.age);
 	}
 }
--- code/corejava/workspace/package-acess-modifier/src/student/StudentInfo.java
+++ code/corejava/workspace/package-acess-modifier/src/student/StudentInfo.java
@@ -1,5 +1,15 @@
 package student;
 
 public class StudentInfo {
-
+	public int id;
+	protected String name;
+	String contact;
+	private int age;
+	
+	public void print() {
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(contact);
+		System.out.println(age);
+	}
 }
