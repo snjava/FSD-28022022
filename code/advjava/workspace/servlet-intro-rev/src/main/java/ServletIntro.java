28e0e9e05b1100d5e8470fba5cec9d4d83bad841
snjava <scnaikwadi2@gmail.com>
2022-07-04 11:02:31 +0530

    Servlet : Return response to user
    Servlet : Return response to user

---
+++ code/advjava/workspace/servlet-intro-rev/src/main/java/ServletIntro.java
@@ -0,0 +1,24 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet-intro")
+public class ServletIntro extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello This is a First Servlet");
+		
+		response.setContentType("text/html"); // specify the type of response
+		PrintWriter out = response.getWriter(); // create object of printwriter to return response
+		out.print("<h1>Welcome To First Servlet Response in HTML</h1>"); // response to the client
+		out.print("Enter Message : <input type='text' name='msg'/>");
+		out.print("<button type='submit'>Send</button>");
+	}
+
+}
