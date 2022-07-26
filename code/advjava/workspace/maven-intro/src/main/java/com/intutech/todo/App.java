d87ffcb28d346c29796a07e7327048d08a28f2b5
snjava <scnaikwadi2@gmail.com>
2022-07-26 11:02:45 +0530

    Maven Intro
    Maven Intro

---
+++ code/advjava/workspace/maven-intro/src/main/java/com/intutech/todo/App.java
@@ -0,0 +1,13 @@
+package com.intutech.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
